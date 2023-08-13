92795c531832d5440a0388cfdd758777e87b094e
Evhen Korolov <fricker12@gmail.com>
2023-08-13 23:03:50 +0300

    add port

--- app/__init__.py
+++ app/__init__.py
@@ -3,30 +3,34 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 app = Flask(__name__)
 
 # Простой список для хранения пар пользователей
-users = [{'username': 'admin', 'password': 'admin'}]
+users = [{"username": "admin", "password": "admin"}]
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
 
         for user in users:
-            if user['username'] == username and user['password'] == password:
-                return redirect(url_for('birthday'))
-        
-        return "Неверное имя пользователя или пароль"
-    
-    return render_template('login.html', dark_theme=False)
+            if user["username"] == username and user["password"] == password:
+                return redirect(url_for("birthday"))
 
-@app.route('/toggle-theme', methods=['POST'])
+        return "Неверное имя пользователя или пароль"
+
+    return render_template("login.html", dark_theme=False)
+
+
+@app.route("/toggle-theme", methods=["POST"])
 def toggle_theme():
-    dark_theme = request.json.get('dark_theme', False)
+    dark_theme = request.json.get("dark_theme", False)
     return jsonify(success=True, dark_theme=dark_theme)
 
-@app.route('/birthday')
-def birthday():
-    return render_template('birthday.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/birthday")
+def birthday():
+    return render_template("birthday.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=5000)
